eb225d5c212984cff3a3f960f5b0ad06481338ed
lukilme <kilmerluisluk12@gmail.com>
2024-12-14 11:25:45 -0300

    test1

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def scrape_data():
+    url = "https://github.com/lukilme?tab=repositories"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.title.string if soup.title else "No title found"
+    
+    description_tag = soup.find('meta', attrs={"name": "description"})
+    description = description_tag.get('content') if description_tag else "No description found"
+    
+    repositories = []
+    repo_elements = soup.find_all('a', attrs={"itemprop": "name codeRepository"})
+    for repo in repo_elements:
+        repositories.append(repo.get('href'))  
+
+    data = {
+        "title": title,
+        "description": description,
+        "repositories": repositories
+    }
+
+    with open('data.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+if __name__ == "__main__":
+    scrape_data()
